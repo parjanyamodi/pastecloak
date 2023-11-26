67c715cb4c54389a4f304ecb999c1e851a73a657
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2023-11-26 23:22:03 +0530

    CHORE: ui init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
 import type { Config } from 'tailwindcss'
-
+import { nextui } from '@nextui-org/react'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
+    screens: {
+      '2xs': '350px',
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -15,6 +26,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+
+  plugins: [nextui()],
 }
 export default config
